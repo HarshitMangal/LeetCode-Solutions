e15f38d916eeb8418052d0583c54d231e44bfcfb
Harshit Mangal <mangalharsh54@gmail.com>
2026-06-26 15:21:34 +0530

    Time: 44 ms (96.49%), Space: 119.8 MB (98.5%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.cpp
+++ 3484-design-spreadsheet/3484-design-spreadsheet.cpp
@@ -1,52 +1,63 @@
 class Spreadsheet {
 public:
 
-   unordered_map<string,int> mp;
+    unordered_map<string, int> mp;
+
     Spreadsheet(int rows) {
-        
+
     }
-    
+
     void setCell(string cell, int value) {
-          mp[cell]=value;
+        mp[cell] = value;
     }
-    
-    int get(string &s){
-        if(isdigit(s[0])){
+
+    void resetCell(string cell) {
+        mp.erase(cell);
+    }
+
+    int getOperandValue(string s)
+    {
+        if(isdigit(s[0]))
             return stoi(s);
-        }
-         if(mp.count(s))
-        return mp[s];
+
+        if(mp.count(s))
+            return mp[s];
 
         return 0;
     }
-    void resetCell(string cell) {
-            mp.erase(cell);
-    }
-    
+
     int getValue(string formula) {
-         string left = "";
-            string right = "";
 
-            int i = 1;
+        string left = "";
+        string right = "";
 
-            // Left operand
-            while(formula[i] != '+')
-            {
-                left += formula[i];
-                i++;
-            }
+        int i = 1; // '=' skip
 
-            i++; // '+' skip
+        while(formula[i] != '+')
+        {
+            left += formula[i];
+            i++;
+        }
 
-            // Right operand
-            while(i < formula.size())
-            {
-                right += formula[i];
-                i++;
-            }
-            return get(left)+get(right);
+        i++; // '+' skip
+
+        while(i < formula.size())
+        {
+            right += formula[i];
+            i++;
+        }
+
+        return getOperandValue(left) + getOperandValue(right);
     }
 };
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
 
 /**
  * Your Spreadsheet object will be instantiated and called as such:
