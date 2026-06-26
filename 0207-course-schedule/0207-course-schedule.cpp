fa20ddc6064e7aea522669b7d9b043830ff1b07c
Harshit Mangal <mangalharsh54@gmail.com>
2026-06-26 13:34:18 +0530

    Time: 9 ms (27.06%), Space: 20.1 MB (24.57%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>&edges) {
+        vector<int>indegree(n,0);
+        vector<vector<int>>adj(n);
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+          int count=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            count++;
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return count==n;
+    }
+};
