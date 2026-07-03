3c891f93f23739a368482e4c17109e16faeca0fb
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-03 16:55:25 +0530

    Time: 7 ms (2.7%), Space: 26.1 MB (3.03%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& arr) {
+            int n=arr.size();
+         sort(arr.rbegin(),arr.rend());
+         int i=0;
+         int count=0;
+         while(i<n&&arr[i]>i){
+            count++;
+            i++;
+         }
+
+         return count;
+    }
+};
