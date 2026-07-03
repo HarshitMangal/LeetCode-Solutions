62f6b011531caead184ff25370b685ab7855764e
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-03 16:02:36 +0530

    Time: 243 ms (66.8%), Space: 342.2 MB (62.45%) - LeetHub

---
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    using ll = long long;
+    const ll INF = 1e18;
+
+    bool dijkstra(vector<vector<pair<int,int>>> &graph, int mid, ll k, int n) {
+
+        vector<ll> dist(n, INF);
+
+        priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> pq;
+
+        dist[0] = 0;
+        pq.push({0,0});
+
+        while(!pq.empty()){
+
+            auto [d,u] = pq.top();
+            pq.pop();
+
+            if(d > dist[u]) continue;
+
+            for(auto &[v,w] : graph[u]){
+
+                if(w < mid) continue;
+
+                if(dist[v] > dist[u] + w){
+                    dist[v] = dist[u] + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+
+        return dist[n-1] <= k;
+    }
+
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+
+        int n = online.size();
+
+        vector<vector<pair<int,int>>> graph(n);
+
+        int low = INT_MAX;
+        int high = 0;
+
+        for(auto &e : edges){
+
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            low = min(low, w);
+            high = max(high, w);
+
+            if(u != 0 && u != n-1 && !online[u]) continue;
+            if(v != 0 && v != n-1 && !online[v]) continue;
+
+            graph[u].push_back({v,w});
+        }
+
+        int ans = -1;
+
+        while(low <= high){
+
+            int mid = low + (high-low)/2;
+
+            if(dijkstra(graph, mid, k, n)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
