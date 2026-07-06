f1cc54ec0941b991e3d394a3e4e5ddbfe489527b
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-07 02:28:27 +0530

    Time: 565 ms (67.16%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a
+JOIN (
+    SELECT player_id,
+           MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) t
+ON a.player_id = t.player_id
+WHERE DATEDIFF(a.event_date, t.first_login) = 1;
