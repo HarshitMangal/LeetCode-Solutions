6dabbfd1ec3e5e2a422bf9ccf043d978a64d957f
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-09 02:14:16 +0530

    Time: 274 ms (80.37%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-SELECT 
-ifnull(
-    (select distinct salary 
+select (select distinct salary 
 FROM Employee
 ORDER BY salary DESC
-LIMIT 1 OFFSET 1),NULL)as SecondHighestSalary
+LIMIT 1 OFFSET 1) as SecondHighestSalary
