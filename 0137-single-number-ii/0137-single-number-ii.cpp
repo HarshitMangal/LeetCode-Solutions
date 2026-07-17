cec3d85b492583bddd767803ec15183bdae4c457
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-17 13:09:01 +0530

    Time: 3 ms (31.39%), Space: 13.4 MB (64.16%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     int ans = 0;
+
+        // Har bit check karenge
+        for (int i = 31; i >= 0; i--) {
+
+            int cnt = 0;
+
+            // Sabhi numbers ki i-th bit count karo
+            for (int num : nums) {
+
+                int ithbit = (num >> i) & 1;
+
+                if (ithbit == 1)
+                    cnt++;
+            }
+
+            // Agar remainder bacha to ye unique number ki bit hai
+            if (cnt % 3 != 0) {
+                ans |= (1 << i);
+            }
+        }
+
+        return ans;
+    }
+};
