00541e1a2cb036eb298d6522b1a9c0516fe73985
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-20 21:37:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (52.44%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        
+        int n=s.length();
+        bool flag=false;
+        int a=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='A') a++;
+        }
+        if(a>=2) return false;
+        int count=1;
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='L'&&s[i]==s[i+1]){
+                count++;
+            }
+            if(count>=3) return false;
+            else if(s[i]!='L') count=1;
+        }
+        return true;
     }
 };
