8af62a2bcd5bd21d02dc52e6751572f4c9751b5c
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-22 03:15:23 +0530

    Time: 582 ms (25.07%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,8 +1,6 @@
-SELECT p.project_id,
-       ROUND(
-           IFNULL(SUM(e.experience_years) / COUNT(e.experience_years), 0),
-           2
-       ) AS average_years
+SELECT
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) AS average_years
 FROM Project p
 JOIN Employee e
 ON p.employee_id = e.employee_id
