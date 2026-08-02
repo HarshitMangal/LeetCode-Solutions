61f5086f236c4790d8933b7562d1ab85cd8b34dd
Harshit Mangal <mangalharsh54@gmail.com>
2026-08-02 13:27:44 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int j) {
-
-        if (i == j)
-            return nums[i];
-
-        int takeLeft = nums[i] - solve(nums, i + 1, j);
-
-        int takeRight = nums[j] - solve(nums, i, j - 1);
-
-        return max(takeLeft, takeRight);
-    }
-    bool predictTheWinner(vector<int>& nums) {
-         return solve(nums, 0, nums.size() - 1) >= 0;
+    int solve(vector<int>& arr, vector<vector<int>>& dp, int i, int j) {
+      if (i > j) return 0;  // base case: invalid subarray
+      if (dp[i][j] != -1) return dp[i][j];
+      
+      int pickLeft = arr[i] + min(
+          solve(arr, dp, i + 2, j),
+          solve(arr, dp, i + 1, j - 1)
+      );
+      
+      int pickRight = arr[j] + min(
+          solve(arr, dp, i + 1, j - 1),
+          solve(arr, dp, i, j - 2)
+      );
+      
+      return dp[i][j] = max(pickLeft, pickRight);
+   }
+    bool predictTheWinner(vector<int>& piles) {
+             int n=piles.size();
+         int sum=accumulate(piles.begin(),piles.end(),0);
+         vector<vector<int>> dp(n, vector<int>(n, -1));
+        int player1_score=solve(piles,dp,0,n-1);
+        int player2_score=sum-player1_score;
+        return player1_score>=player2_score;
         
     }
 };
