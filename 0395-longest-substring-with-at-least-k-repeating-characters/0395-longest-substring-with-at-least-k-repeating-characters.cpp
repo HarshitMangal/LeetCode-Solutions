3ebf098f756df20a9db60599e4e12c1bea1dcd22
Harshit Mangal <mangalharsh54@gmail.com>
2026-08-12 10:40:13 +0530

    Time: 276 ms (17.47%), Space: 10.9 MB (38.91%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int n=s.length();
-        //sabse phle btrute force se karnege
-        int ans=0;
-        for(int i=0;i<n;i++){
-            unordered_map<char,int>mp;
-            for(int j=i;j<n;j++){
-                mp[s[j]]++;
-                bool flag=true;
-                for(auto it:mp){
-                    if(it.second<k){
-                        flag=false;
+        int n = s.size();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            vector<int> freq(26, 0);
+
+            for(int j = i; j < n; j++) {
+
+                freq[s[j] - 'a']++;
+
+                // Check whether every present character
+                // occurs at least k times
+                bool valid = true;
+
+                for(int x = 0; x < 26; x++) {
+                    if(freq[x] > 0 && freq[x] < k) {
+                        valid = false;
                         break;
                     }
                 }
-                if(flag==true){
-                    ans=max(ans,j-i+1);
-                }
 
+                if(valid) {
+                    ans = max(ans, j - i + 1);
+                }
             }
         }
+
         return ans;
-        
-        
     }
 };
