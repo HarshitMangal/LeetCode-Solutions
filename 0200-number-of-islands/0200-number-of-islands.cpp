e330121102f47bf48c1b012b4699abbc9f0674d2
Harshit Mangal <mangalharsh54@gmail.com>
2026-08-22 23:43:39 +0530

    Time: 20 ms (94.12%), Space: 16.3 MB (70.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-   void  solve(vector<vector<char>>&mat,int i,int j,vector<vector<bool>>&visited){
-       int n=mat.size();
-       int m=mat[0].size();
-       if(i<0||i>=n||j<0||j>=m||mat[i][j]=='0'||visited[i][j]==true) return ;
+    void solve(vector<vector<char>>&mat,int i,int j){
+        int n=mat.size();
+        int m=mat[0].size();
+        if(i<0||i>=n||j<0||j>=m||mat[i][j]=='0') return ;
+        mat[i][j]='0';
+       solve(mat,i+1,j);
+       solve(mat,i-1,j);
+       solve(mat,i,j+1);
+       solve(mat,i,j-1);
 
-        visited[i][j]=true;
-       solve(mat,i+1,j,visited);
-       solve(mat,i,j+1,visited);
-       solve(mat,i,j-1,visited);
-       solve(mat,i-1,j,visited);
-   }
+    }
     int numIslands(vector<vector<char>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-           vector<vector<bool>>visited(n,vector<bool>(m,false));
-           int count=0;
+        int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               if(mat[i][j]=='1'&&!visited[i][j]){
-                solve(mat,i,j,visited);
-                count++;
-               }
+                if(mat[i][j]=='1'){
+                    solve(mat,i,j);
+                    ans++;
+                }
+
             }
         }
-        return count;
+         return ans;
     }
 };
