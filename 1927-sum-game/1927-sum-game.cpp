3c6143a127dff15c112d1fd670489523b6451050
Harshit Mangal <mangalharsh54@gmail.com>
2026-08-23 23:45:11 +0530

    Time: 0 ms (100%), Space: 13.9 MB (92.07%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for (int i = 0; i < n / 2; ++i) {
+            if (num[i] == '?') {
+                q1++;
+            } else {
+                sum1 += num[i] - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; ++i) {
+            if (num[i] == '?') {
+                q2++;
+            } else {
+                sum2 += num[i] - '0';
+            }
+        }
+
+     
+        return (sum1 - sum2) * 2 + (q1 - q2) * 9 != 0;
+    }
+};
