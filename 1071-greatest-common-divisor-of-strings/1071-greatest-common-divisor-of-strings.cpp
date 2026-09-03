6706b041c5539fc30a8184710082438761592a66
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-03 10:54:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (14.69%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     string gcdOfStrings(string s1, string s2) {
         if(s1+s2!=s2+s1) return "";
-        int len=gcd(s1.length(),s2.length());
-        return s1.substr(0,len);
+         int n=s1.length();
+        int m=s2.length();
+        int ans=__gcd(n,m);
+        return s1.substr(0,ans);
     }
 };
