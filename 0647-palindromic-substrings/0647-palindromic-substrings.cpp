ea67abbb98987efc837fe4af23060f3f8e3bdb0b
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-06 12:14:20 +0530

    Time: 367 ms (19.28%), Space: 8.7 MB (59.69%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   bool ispal(string &s,int i,int j){
+    int n=s.length();
+    while(i<j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+   }
+    int countSubstrings(string s) {
+        int n=s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(ispal(s,i,j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+};
