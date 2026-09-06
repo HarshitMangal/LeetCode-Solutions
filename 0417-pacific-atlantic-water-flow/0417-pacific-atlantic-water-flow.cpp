c2ffa361624fc5f78b12285128a2abf79c36253d
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-07 01:37:04 +0530

    Time: 7 ms (56.59%), Space: 22.1 MB (88.01%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
+    vector<int>dx={1,-1,0,0};
+    vector<int>dy={0,0,1,-1};
     void dfs(vector<vector<int>>&mat,int i,int j,vector<vector<bool>>&visited){
         int n=mat.size();
         int m=mat[0].size();
-        if(i<0||i>=n||j<0||j>=m||visited[i][j]==true) return;
+        if(i<0||i>=n||j<0||j>=m||visited[i][j]==true) return ;
         visited[i][j]=true;
-        vector<int>dx={1,-1,0,0};
-        vector<int>dy={0,0,1,-1};
         for(int k=0;k<4;k++){
             int nx=i+dx[k];
             int ny=j+dy[k];
@@ -16,27 +16,33 @@ public:
         }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-         vector<vector<int>>ans;
-         vector<vector<bool>>pacific(n,vector<bool>(m,false));
-         vector<vector<bool>>atlantic(n,vector<bool>(m,false));
-         //pac ke liye
-         for(int i=0;i<n;i++){
+           vector<vector<int>>ans;
+           int n=mat.size();
+           int m=mat[0].size();
+           vector<vector<bool>>pacific(n,vector<bool>(m,false));
+           vector<vector<bool>>atlantic(n,vector<bool>(m,false));
+           //phle pacific ka
+           for(int i=0;i<m;i++){
+            dfs(mat,0,i,pacific);
+            dfs(mat,n-1,i,atlantic);
+           }
+           for(int i=0;i<n;i++){
             dfs(mat,i,0,pacific);
             dfs(mat,i,m-1,atlantic);
-         }
-         for(int j=0;j<m;j++){
-            dfs(mat,0,j,pacific);
-            dfs(mat,n-1,j,atlantic);
-         }
-         for(int i=0;i<n;i++){
+           }
+           for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(pacific[i][j]==true&&atlantic[i][j]==true){
-                     ans.push_back({i,j});
+                    ans.push_back({i,j});
                 }
             }
-         }
-         return ans;
+           }
+           return ans;
+
+
+           
+
+
+           
     }
 };
