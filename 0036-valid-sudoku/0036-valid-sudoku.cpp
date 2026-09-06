29fdfc1c13517af3420f79ded2057f2c7bfa34fe
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-06 14:52:15 +0530

    Time: 0 ms (100%), Space: 21.4 MB (96.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-      bool canplace(vector<vector<char>>&mat,int r,int c,int num){
+    bool canplace(vector<vector<char>>&mat,int r,int c,int num){
+    int n=mat.size();
+    int m=mat[0].size();
+    //ab hum check karnege yar
+    for(int i=0;i<n;i++){
+        if(mat[r][i]==num+'0') return false;
+    }
+    for(int i=0;i<n;i++){
+        if(mat[i][c]==num+'0') return false;
+    }
+    int x=(r/3)*3;
+    int y=(c/3)*3;
+    for(int i=x;i<x+3;i++){
+        for(int j=y;j<y+3;j++){
+            if(mat[i][j]==num+'0')return false;
+        }
+    }
+    return true;
+
+
+   }
+    bool isValidSudoku(vector<vector<char>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        for(int i=0;i<9;i++){
-            if(mat[r][i]==(num+'0')) return false;
-            if(mat[i][c]==(num+'0')) return false;
-        }
-        
-     int x=(r/3)*3;
-     int y=(c/3)*3;
-     for(int i=x;i<x+3;i++){
-        for(int j=y;j<y+3;j++){
-            if(mat[i][j]==(num+'0')) return false;
-        }
-     }
-     return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& mat) {
-         int n=mat.size();
-         int m=mat[0].size();
-         for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]!='.'){
-                    int temp=mat[i][j]-'0';
-                    mat[i][j]='.';
-                    if(canplace(mat,i,j,temp)==false) return false;
-                    mat[i][j]=temp+'0';
-                }
-
-
+                int num=mat[i][j]-'0';
+                mat[i][j]='.';
+                if(canplace(mat,i,j,num)==false) return false;;
+                mat[i][j]=num+'0';
             }
-         }
-         return true;
+        }
+    }
+        return true;
     }
 };
