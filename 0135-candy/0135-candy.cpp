5839d02abfc75baf7575f8fdea25b752ea578507
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-10 01:36:36 +0530

    Time: 11 ms (9.46%), Space: 71.2 MB (31.05%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int candy(vector<int>& arr) {
         int n=arr.size();
-        //two pass me hoga ye 
         vector<int>ans(n,1);
-        int count=0;
-        //left pass me
+        int sum=0;
+        //phle left se
         for(int i=1;i<n;i++){
-          if(arr[i]>arr[i-1]){
-            ans[i]=(ans[i-1]+1);
-          }
+            if(arr[i]>arr[i-1]){
+                ans[i]=ans[i-1]+1;
+            }
         }
         for(int i=n-2;i>=0;i--){
             if(arr[i]>arr[i+1]){
@@ -17,9 +16,10 @@ public:
             }
         }
         for(int i=0;i<n;i++){
-            count+=ans[i];
+            sum+=ans[i];
         }
-        return count;
+        return sum;
+
         
     }
 };
