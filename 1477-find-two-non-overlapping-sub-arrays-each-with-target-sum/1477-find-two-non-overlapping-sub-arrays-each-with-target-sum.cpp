7abd8fb430adf2ad604138ffb65ff0a7015019db
Harshit Mangal <mangalharsh54@gmail.com>
2026-09-19 00:02:39 +0530

    Time: 12 ms (57.25%), Space: 92.3 MB (47.33%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,61 +1,43 @@
 class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
-        int n = arr.size();
-
-        vector<int> prefix(n, 1e9);
-        vector<int> suffix(n, 1e9);
-
-        // prefix
-        int left = 0;
-        int sum = 0;
-        int best = 1e9;
-
-        for (int right = 0; right < n; right++) {
-            sum += arr[right];
-
-            while (sum > target) {
-                sum -= arr[left];
-                left++;
-            }
-
-            if (sum == target) {
-                int len = right - left + 1;
-                best = min(best, len);
-            }
-
-            prefix[right] = best;
-        }
-
-        // suffix
-        left = n - 1;
-        sum = 0;
-        best = 1e9;
-
-        for (int right = n - 1; right >= 0; right--) {
-            sum += arr[right];
-
-            while (sum > target) {
-                sum -= arr[left];
-                left--;
-            }
-
-            if (sum == target) {
-                int len = left - right + 1;
-                best = min(best, len);
-            }
-
-            suffix[right] = best;
-        }
-
-        int ans = 1e9;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (prefix[i] != 1e9 && suffix[i + 1] != 1e9) {
-                ans = min(ans, prefix[i] + suffix[i + 1]);
-            }
-        }
-
-        return ans == 1e9 ? -1 : ans;
+             int n=arr.size();
+             vector<int>prefix(n,INT_MAX);
+             vector<int>suffix(n,INT_MAX);
+             int best=INT_MAX;
+             int i=0;
+             int sum=0;
+             for(int j=0;j<n;j++){
+                sum+=arr[j];
+                while(sum>target){
+                    sum-=arr[i];
+                    i++;
+                }
+                if(sum==target){
+                    best=min(best,j-i+1);
+                }
+                prefix[j]=best;
+             }
+             sum=0;
+             i=n-1;
+             best=INT_MAX;
+             for(int j=n-1;j>=0;j--){
+                sum+=arr[j];
+                while(sum>target){
+                    sum-=arr[i];
+                    i--;
+                }
+                if(sum==target){
+                    best=min(best,i-j+1);
+                }
+                suffix[j]=best;
+             }
+             int ans=INT_MAX;
+             for(int i=0;i<n-1;i++){
+                if(prefix[i]!=INT_MAX&&suffix[i+1]!=INT_MAX){
+                    ans=min(ans,prefix[i]+suffix[i+1]);
+                }
+             }
+             return ans==INT_MAX?-1:ans;
     }
 };
